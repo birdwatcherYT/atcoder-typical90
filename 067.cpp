03f0e1044250cff659ffd4401395a665d8766e8a
birdwatcherYT <aabcehmu.362880@hotmail.co.jp>
2021-10-31 22:47:51 +0900

    add 067

---
+++ 067.cpp
@@ -0,0 +1,165 @@
+#pragma GCC optimize("O3")
+// #include <atcoder/all>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <climits>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <cassert> // assert();
+#include <iomanip> // cout << setprecision(15);
+// #include <bits/stdc++.h>
+#ifndef M_PI
+	#define M_PI		3.14159265358979323846
+#endif
+#ifndef M_E
+	#define M_E		2.7182818284590452354
+#endif
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+#define REP(i, n)  FOR(i, 0, n)
+#define RREP(i, n)  for(int i = (n) - 1; i >= 0; --i)
+#define EACH(e, v) for(auto &e : v)
+#define ITR(it, v) for(auto it = (v).begin(); it != (v).end(); ++it)
+#define RITR(it, v) for(auto it = (v).rbegin(); it != (v).rend(); ++it)
+#define CASE(x) break; case x:
+#define ALL(v)  (v).begin(), (v).end()
+#define RALL(v) (v).rbegin(), (v).rend()
+#define SZ(v) int((v).size())
+#define EXIST(s, e) ((s).find(e) != (s).end())
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define SUM(v, type) accumulate((v).begin(), (v).end(), (type) 0)
+#define MIN(v) (*min_element((v).begin(), (v).end()))
+#define MAX(v) (*max_element((v).begin(), (v).end()))
+#define ARGMIN(v) (min_element((v).begin(), (v).end()) - (v).begin())
+#define ARGMAX(v) (max_element((v).begin(), (v).end()) - (v).begin())
+#define REVERSE(v) reverse((v).begin(), (v).end())
+#define ARANGE(v) iota((v).begin(), (v).end(), 0)
+#define FILTER(src, tgt, func) copy_if(begin(src), end(src), back_inserter(tgt), func); // func = [](type x){return 条件;}
+#define CTOI(c) (c - '0')
+#define HEADSTR(str, n) str.substr(0, (n))
+#define TAILSTR(str, n) str.substr((str).length() - (n))
+#define CONTAINS(str, c) ((str).find(c) != string::npos)
+#define INSPOS(v, a) (lower_bound((v).begin(), (v).end(), a) - (v).begin())
+// io系
+#define OUT(x) cout << (x) << endl;
+#define IN(x) cin >> x;
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+using namespace std;
+
+template<class T> vector<size_t> argsort(const vector<T> &vec, bool asc=true){
+	vector<size_t> index(vec.size());
+	iota(index.begin(), index.end(), 0);
+	sort(index.begin(), index.end(), [&vec, &asc](size_t i, size_t j){return asc ? (vec[i] < vec[j]):(vec[i] > vec[j]);});
+	return index;
+}
+// 表示系
+template <class Head> void print(Head&& head) {
+	cout << head << endl;
+}
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {
+	cout << head << " ";
+	print(forward<Tail>(tail)...);
+}
+template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1, T2>& p) {
+	os << "(" << p.first << ", " << p.second << ")";
+	return os;
+}
+template<class T> ostream& operator<<(ostream& os, const vector<T>& vec) {
+	os << "[ ";
+	for ( const T& item : vec ) os << item << ", ";
+	os << "]";
+	return os;
+}
+template<class T> ostream& operator<<(ostream& os, const set<T>& s) {
+	os << "{ ";
+	for ( const T& item : s ) os << item << ", ";
+	os << "}";
+	return os;
+}
+template<class T1, class T2> ostream& operator<<(ostream& os, const map<T1, T2>& m) {
+	os << "{ ";
+	for ( const auto &[key, value] : m ) os << key << ":"<< value << ", ";
+	os << "}";
+	return os;
+}
+// 入力系
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1, T2>& p) {
+	is >> p.first >> p.second;
+	return is;
+}
+template<class T> istream& operator>>(istream& is, vector<T>& vec) {
+	for ( T& item : vec ) is >> item;
+	return is;
+}
+// 集合演算
+template<class T> set<T> operator&(const set<T>& a, const set<T>& b) {// 共通集合
+	set<T> ans;
+	set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(ans, ans.end()));
+	return ans;
+}
+template<class T> set<T> operator|(const set<T>& a, const set<T>& b) {// 和集合
+	set<T> ans;
+	std::set_union(a.begin(), a.end(), b.begin(), b.end(), std::inserter(ans, ans.end()));
+	return ans;
+}
+template<class T> set<T> operator-(const set<T>& a, const set<T>& b) {// 差集合
+	set<T> ans;
+	std::set_difference(a.begin(), a.end(), b.begin(), b.end(), std::inserter(ans, ans.end()));
+	return ans;
+}
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef set<int> SI;
+typedef long long LL;
+
+istringstream debug_iss(R"(
+21 1
+)");
+// #define cin debug_iss
+
+// int -> str: to_string(i)
+// str -> int: stoi(s)
+// vec -> set: set<int> s(ALL(v));
+int main() {
+	string n_str8;
+	IN(n_str8)
+	int k;
+	IN(k)
+
+	REP(_, k){
+		LL n10=0;
+		LL base=1;
+		RREP(i, SZ(n_str8)){// 8進数 -> 10進数
+			n10+=base*CTOI(n_str8[i]);
+			base*=8;
+		}
+		// 10進数 -> 9進数
+		string n9_str="";
+		do{
+			n9_str=to_string(n10%9)+n9_str;
+			n10/=9;
+		}while(n10!=0);
+		// 8を5に書き換える
+		REP(i, SZ(n9_str)){
+			if (n9_str[i]=='8')
+				n9_str[i]='5';
+		}
+		n_str8=n9_str;
+		// dump(n_str8)
+	}
+	OUT(n_str8)
+	return 0;
+}
